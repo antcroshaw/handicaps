1bf964bde90781ac602c0c99cd27ef9f5c1ee62e
Anthony Croshaw <anthonycroshaw@anthonys-air.home>
2021-08-06 14:30:53 +0100

    got router link working

--- src/components/Handicaps.vue
+++ src/components/Handicaps.vue
@@ -1,13 +1,25 @@
 <template>
 <h1>Handicaps</h1>
+ <div v-for="handicap in handicaps" :key="handicap.id">
+   <router-link :to="{
+     name: 'handicaps',
+     params: handicap.id
+   }">{{ handicap.name }}</router-link>
+ </div>
+  <router-view></router-view>
 </template>
-
 <script>
 export default {
-  name: 'Handicaps'
+  data () {
+    return {
+      handicaps: [
+        { id: 1, name: 'york' },
+        { id: 2, name: 'western' }
+      ]
+    }
+  }
 }
 </script>
-
 <style scoped>
 
 </style>
