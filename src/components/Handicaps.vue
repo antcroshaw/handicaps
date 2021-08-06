ef1a3e7dd11d7ca83cd8430c9ab2f3306b7d480a
Anthony Croshaw <anthonycroshaw@anthonys-air.home>
2021-08-06 09:47:59 +0100

    updated router

---
+++ src/components/Handicaps.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Handicaps</h1>
+</template>
+
+<script>
+export default {
+  name: 'Handicaps'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../components/Home'
+import Handicaps from '../components/Handicaps'
+import About from '../components/About'
 
 const routes = [
   {
@@ -8,12 +10,14 @@ const routes = [
     component: Home
   },
   {
+    path: '/handicaps',
+    name: 'handicaps',
+    component: Handicaps
+  },
+  {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'about',
+    component: About
   }
 ]
 
